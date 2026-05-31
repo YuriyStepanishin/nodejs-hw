1d37d511f69b6532c0a8d8107ad9bad37fccf0ce
YuriyStepanishin <y.stepanishin@gmail.com>
2026-05-31 15:43:13 +0300

    hw-04

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -16,7 +16,11 @@ import {
 
 import { celebrate } from 'celebrate';
 
+import { authenticate } from '../middleware/authenticate.js';
+
 const router = Router();
+
+router.use('/notes', authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,6 +21,9 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
+app.use(cookieParser());
+
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(errors());
