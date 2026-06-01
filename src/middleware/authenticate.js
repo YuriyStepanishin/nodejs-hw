84c9934dcd88b630fdf47e3e5c2add8644da6c37
YuriyStepanishin <y.stepanishin@gmail.com>
2026-06-01 20:22:20 +0300

    fix authenticate & model index

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,28 +3,28 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { accessToken } = req.cookies;
+  const { accessToken, sessionId } = req.cookies;
 
-  if (!accessToken) {
+  if (!accessToken || !sessionId) {
     throw createHttpError(401, 'Missing access token');
   }
 
-  const session = await Session.findOne({
-    accessToken,
-  });
+  const session = await Session.findOne({ _id: sessionId, accessToken });
 
   if (!session) {
     throw createHttpError(401, 'Session not found');
   }
 
-  if (session.accessTokenValidUntil < new Date()) {
+  const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
+
+  if (isAccessTokenExpired) {
     throw createHttpError(401, 'Access token expired');
   }
 
   const user = await User.findById(session.userId);
 
   if (!user) {
-    throw createHttpError(401);
+    throw createHttpError(401, 'User not found');
   }
 
   req.user = user;
